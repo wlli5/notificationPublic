44f803e769e93fa87666b5ac622d0bb09a8fbc84
Ubuntu <ubuntu@ip-172-31-80-196.ec2.internal>
2020-02-07 22:05:25 +0000

    test

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,47 +11,9 @@ class MoviesController < ApplicationController
   @all_ratings = Movie.ratings
   end
   
-    def index
-    @all_ratings = Movie.all_ratings
-    sort = params[:sort]
-    hash = Hash.new
-    use_ratings_session = !params[:ratings].present? && session[:ratings].present? 
-    use_sort_session = !params[:sort].present? && session[:sort].present?
-    
-    if use_ratings_session || use_sort_session
-      if use_ratings_session && use_sort_session
-        hash = {:ratings=>session[:ratings], :sort=>session[:sort]}
-      elsif use_ratings_session
-        hash = {:ratings=>session[:ratings]}
-      else 
-        hash = {:sort=>session[:sort]}
-      end
-      flash.keep
-      redirect_to movies_path(params.merge(hash))
-    end
-    
-      
-    if params[:ratings].present?
-      @selected_ratings =  params[:ratings].keys
-      @movies = Movie.where(rating: params[:ratings].keys)
-    else
-      @selected_ratings = @all_ratings
-      @movies = Movie.all
-    end
-
-    
-    if sort == "title"
-      @movies = @movies.order(:title)
-      @css_title = "hilite"
-    elsif sort == "release_date"
-      @movies = @movies.order(:release_date)
-      @css_release_date = "hilite"
-    end
-    
-    session[:ratings] = params[:ratings]
-    session[:sort] = params[:sort]
+  def index
+    @movies = Movie.all
   end
-
 
   def new
     # default: render 'new' template
