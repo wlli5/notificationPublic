ee44056984802815bf0c39605b5a0601a4aedc39
Ubuntu <ubuntu@ip-172-31-80-196.ec2.internal>
2020-02-08 04:29:46 +0000

    test

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -15,6 +15,20 @@ class MoviesController < ApplicationController
     @movies = Movie.all
     rate = params[:ratings]
     sorted = params[:sort]
+    use_ratings_session = !params[:ratings].present? && session[:ratings].present? 
+    use_sort_session = !params[:sort].present? && session[:sort].present?
+    
+    if use_ratings_session || use_sort_session
+      if use_ratings_session && use_sort_session
+        hash = {:ratings=>session[:ratings], :sort=>session[:sort]}
+      elsif use_ratings_session
+        hash = {:ratings=>session[:ratings]}
+      else 
+        hash = {:sort=>session[:sort]}
+      end
+      flash.keep
+      redirect_to movies_path(params.merge(hash))
+    end
     if rate.present?
       @selected_ratings =  rate.keys
       @movies = Movie.where(rating: rate.keys)
