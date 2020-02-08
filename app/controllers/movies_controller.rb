9f15864656bbc6f28f39f2febc3672d1dbba506d
Ubuntu <ubuntu@ip-172-31-80-196.ec2.internal>
2020-02-08 01:13:08 +0000

    test

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,29 +8,11 @@ class MoviesController < ApplicationController
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
-  @all_ratings = Movie.ratings
-  end
-  
-  def index
-        @all_ratings = Movie.all_ratings
-    @movies = Movie.all
-    sort = params[:sort]
-    if params[:ratings].present?
-      @selected_ratings =  params[:ratings].keys
-      @movies = Movie.where(rating: params[:ratings].keys)
-    else
-      @selected_ratings = @all_ratings
-      @movies = Movie.all
-    end
- if sort == "Release_Date"
-    @css_Release_Date = "hilite"
-      @movies = @movies.order(:release_date)
-    elsif sort == "Movie_Title"
-    @css_Movie_Title = "hilite"
-      @movies = @movies.order(:title)
-    end
   end
 
+  def index
+    @movies = Movie.all
+  end
 
   def new
     # default: render 'new' template
@@ -61,4 +43,3 @@ class MoviesController < ApplicationController
   end
 
 end
-
